0c14f0e7bf29c35371cd78818794a50f3d1140fe
dianlujitao <dianlujitao@lineageos.org>
2020-02-02 19:33:37 +0800

    sdm660-common: Explictly build camera HIDL interfaces
    * The 64-bit variants are used by IMS blobs but no longer automatically
      built along with the 32-bit counterparts.
    
    Change-Id: I387e4345c3088bbaf55e24bcfe286ccd877507a6

--- common.mk
+++ common.mk
@@ -90,9 +90,14 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5 \
     camera.device@3.2-impl \
+    vendor.qti.hardware.camera.device@1.0
+
+PRODUCT_PACKAGES += \
     libbson.vendor \
     libxml2 \
     Snap
