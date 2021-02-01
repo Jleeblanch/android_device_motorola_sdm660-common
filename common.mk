9ba9cf604e51094ba5d6d09fb37de406367185f7
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2021-02-01 08:07:51 -0500

    [DNFM]sdm660-common: Enable RRO for overlays, excluding Snap
    Change-Id: Iefa2fd0a65d03482ea0d07470ec3214e7041765e

--- common.mk
+++ common.mk
@@ -36,6 +36,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+PRODUCT_ENFORCE_RRO_TARGETS += *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+   $(LOCAL_PATH)/overlay/packages/apps/Snap
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
