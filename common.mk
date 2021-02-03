5746780a0c7030a166874e0e07b59ff194dfa912
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2021-02-03 18:38:56 -0500

    [DNFM]sdm660-common: Enable RRO overlay for Snap
    Change-Id: Iddea7ef8390131407188f2b5dfbb09d9bdd7282f

--- common.mk
+++ common.mk
@@ -39,8 +39,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-   $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # Audio
 PRODUCT_PACKAGES += \
