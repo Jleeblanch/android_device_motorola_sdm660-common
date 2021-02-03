ef0f7276e7b21d5e3a84e0efa8c8b759a6cfdb0f
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2021-02-03 18:29:42 -0500

    [DNFM]sdm660-common: Split lineage overlays from aosp overlays
    Change-Id: I12d885e5d9097e20a011d59090f8168557774abc

--- common.mk
+++ common.mk
@@ -34,11 +34,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 -include $(LOCAL_PATH)/properties.mk
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-   $(LOCAL_PATH)/overlay/packages/apps/Snap
+   $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # Audio
 PRODUCT_PACKAGES += \
